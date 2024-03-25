d47b8f7e0d2a7aa8ae7bf4512c21eb0d95f084a9
Karthika-PJ <karthikapj12@gmail.com>
2024-03-25 11:50:59 +0530

    added

--- planet-project/index.js
+++ planet-project/index.js
@@ -1,18 +1,29 @@
-const {parse} = require('csv-parse');
+const{parse}  = require('csv-parse');
 const fs = require('fs');
-const results =[];
-fs.createReadStream('kepler_data.csv')
-.pipe(parse({
-    comment:'#',
-    column: true,
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
 }
-))
-.on('data',(data)=>{
-results.push(data)
-}).on('error',(err)=>{
-console.log(err)
-})
-.on('end',()=>{
-    console.log(results);
-    console.log('done');
-})
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+  }))
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(habitablePlanets.map((planet)=>{
+        return planet['kepler_name']
+    }));
+  });
